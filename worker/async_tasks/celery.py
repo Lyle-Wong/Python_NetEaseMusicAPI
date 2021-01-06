4af49fef7f64369f53d75c03fe664b37cda24a10
I318869 <lyle.wang@sap.com>
2021-01-06 19:43:21 +0800

    add celery job

--- worker/async_tasks/celery.py
+++ worker/async_tasks/celery.py
@@ -1,15 +1,11 @@
 import logging
 
 from celery import Celery
-from celery.schedules import crontab
 
 logging.basicConfig(level=logging.INFO,
                 format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S')
 
-
-broker = 'amqp://user:bitnami@stats:5672/solr'
-backend = 'redis://redis:6379/0'
 
 
 # set the default Django settings module for the 'celery' program.
@@ -17,20 +13,6 @@ backend = 'redis://redis:6379/0'
 
 # from django.conf import settings  # noqa
 
-celery_app = Celery('search_event', broker=broker, backend=backend, include=['async_tasks.celery_tasks'])
+celery_app = Celery('search_event')
 
-celery_app.conf.update(
-    result_expires=3600,
-    timezone='Asia/Shanghai',
-    enable_utc=True,
-    beat_schedule={
-        "signin_msg_1": {
-            "task": "celery_app.signin",
-            "schedule": crontab(minute=0, hour=7)
-        },
-        "signin_msg_2": {
-            "task": "celery_app.signin",
-            "schedule": crontab(minute=30, hour=18)
-        }
-    }
-)
+celery_app.config_from_object("async_tasks.celeryconfig")
---
+++ worker/async_tasks/celeryconfig.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2021-01-06 19:35:13
+# @Author  : Your Name (you@example.org)
+# @Link    : link
+# @Version : 1.0.0
+
+import os
+from celery.schedules import crontab
+
+broker_url = 'amqp://user:bitnami@stats:5672/solr'
+result_backend = 'redis://redis:6379/0'
+
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+
+result_expires=3600
+timezone='Asia/Shanghai'
+enable_utc=True
+
+imports = [
+    "async_tasks.celery_tasks",
+]
+
+beat_schedule={
+        "signin_msg_1": {
+            "task": "celery_app.signin",
+            "schedule": crontab(minute=0, hour=7),
+            "args": ()
+        },
+        "signin_msg_2": {
+            "task": "celery_app.signin",
+            "schedule": crontab(minute=30, hour=18),
+            "args": ()
+        }
+    }
